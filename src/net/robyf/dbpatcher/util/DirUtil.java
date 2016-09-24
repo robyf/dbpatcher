aacad3148bea1613a3f8b29c59b99f29f00ae4ab
robyf <rob@robyf.net>
2016-09-24 21:00:23 +0300

    Temp directory created with native java functionality

--- src/net/robyf/dbpatcher/util/DirUtil.java
+++ src/net/robyf/dbpatcher/util/DirUtil.java
@@ -21,6 +21,7 @@ package net.robyf.dbpatcher.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * @since 0.9.0
@@ -32,12 +33,8 @@ public final class DirUtil {
     }
 
     public static File createTempDirectory() {
-        // Kludge due to the lack of native functionality in java
         try {
-            File file = File.createTempFile("dbpatcher", "");
-            file.delete();
-            file.mkdir();
-            return file;
+            return Files.createTempDirectory("dbpatcher").toFile();
         } catch (IOException ioe) {
             throw new UtilException("Error creating a temporary directory", ioe);
         }
